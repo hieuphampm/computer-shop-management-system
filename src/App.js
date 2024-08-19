f65044108fbf3b70c38eccfdd6c138da151f0abf
vina123baov <vothaibao50@gmail.com>
2024-08-19 15:51:12 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AddProduct from "./components/AddProduct";
 import { ProductsContextProvider } from "./global/ProductsContext";
+import Signup from './components/Signup';  // Importing as default exports
+import Login from './components/Login';   // Importing as default exports
+import { auth, db } from './config/Config';
 
 export class App extends Component {
+
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot => {
+          this.setState({
+            user: snapshot.data().Name
+          });
+        });
+      } else {
+        this.setState({
+          user: null
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <ProductsContextProvider>
         <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path='/addproducts' element={<AddProduct />}/>
-          </Routes>
+          <Switch>
+            <Route exact path='/' component={() => <Home user={this.state.user} />} />
+            <Route path='/addproducts' component={AddProducts} />
+            <Route path='/signup' component={Signup} />
+            <Route path='/login' component={Login} />
+          </Switch>
         </BrowserRouter>
       </ProductsContextProvider>
     );
   }
 }
-
-export default App;
